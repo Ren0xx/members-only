6b159e07c8fd44ebe36aff46ae6648301bf048c2
Ren0xx <adamzaleski00@gmail.com>
2023-03-16 11:46:48 +0100

    feat(messageController.js): add input validation to create_message_post route using express-validator fix(messageController.js): sort messages by _id in descending order and limit to 25 in index_get route
    feat(userController.js): add express-validator and errors array to sign-up route render
    
    refactor(userController.js): refactor user_signup_post function to use express-validator for input validation and handle errors properly
    
    style(create-message.ejs): add error message display
    style(layout.ejs): add margin-top to header and main sections
    
    style(sign-up.ejs): remove unnecessary line breaks and spaces
    feat(sign-up.ejs): add error message display for form validation

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,29 +1,49 @@
 const Message = require("../models/message");
 const User = require("../models/user");
 const async = require("async");
+const { body, validationResult } = require("express-validator");
 
 exports.create_message_get = (req, res, next) => {
     if (!req.user) res.redirect("/");
-    res.render("create-message", { title: "Create Message" });
+    res.render("create-message", { title: "Create Message", errors: [] });
 };
-exports.create_message_post = (req, res, next) => {
-    if (!req.user) return;
-    const message = new Message({
-        title: req.body.title,
-        content: req.body.content,
-        owner: req.user.id,
-    });
-    // TODO sanitize etc
-    message
-        .save()
-        .then(() => {
-            res.redirect("/");
-        })
-        .catch((err) => next(err));
-};
+exports.create_message_post = [
+    body("title", "Title must not be empty")
+        .trim()
+        .isLength({ min: 1, max: 70 })
+        .escape(),
+    body("content", "Content must not be empty")
+        .trim()
+        .isLength({ min: 1, max: 255 })
+        .escape(),
+
+    (req, res, next) => {
+        if (!req.user) return;
+        const errors = validationResult(req);
+        const message = new Message({
+            title: req.body.title,
+            content: req.body.content,
+            owner: req.user.id,
+        });
+        if (!errors.isEmpty()) {
+            res.render("create-message", {
+                title: "Create Message",
+                errors: errors.array(),
+            });
+            return;
+        }
+        message
+            .save()
+            .then(() => {
+                res.redirect("/");
+            })
+            .catch((err) => next(err));
+    },
+];
 exports.index_get = (req, res, next) => {
     Message.find({})
-        .sort({ title: 1 })
+        .sort({ _id: -1 })
+        .limit(25)
         .populate("owner")
         .then((messages) => {
             res.render("layout", {
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,11 @@ const User = require("../models/user");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
 // sign-up route - get
 exports.user_create_get = (req, res) => {
     if (req.user) res.redirect("/");
-    res.render("sign-up", { title: "Sign up" });
+    res.render("sign-up", { title: "Sign up", errors: [] });
 };
 // logout - get
 exports.user_logout_get = (req, res, next) => {
@@ -23,28 +24,56 @@ exports.user_signin_get = (req, res) => {
 };
 
 // sign-up - post
-exports.user_signup_post = async (req, res, next) => {
-    const hashPassword = (password) => {
-        return new Promise((resolve, reject) => {
-            bcrypt.hash(password, 10, (err, hashed) => {
-                if (err) reject(err);
-                resolve(hashed);
+exports.user_signup_post = [
+    body("username", "Please enter a valid username").isLength({ min: 5 }),
+
+    // Validate password
+    body("password", "Password should be at least 8 characters long").isLength({
+        min: 8,
+    }),
+
+    // Validate confirm password
+    body("confirmPassword").custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error("Passwords do not match");
+        }
+        return true;
+    }),
+    async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.render("sign-up", { title: "Sign up", errors: errors.array() });
+            return;
+        }
+        const { username } = req.body;
+        const userExists = await User.findOne({ username: username });
+        if (userExists) {
+            errors.errors.push({ msg: "Username already taken" });
+            res.render("sign-up", { title: "Sign up", errors: errors.array() });
+            return;
+        }
+        const hashPassword = (password) => {
+            return new Promise((resolve, reject) => {
+                bcrypt.hash(password, 10, (err, hashed) => {
+                    if (err) reject(err);
+                    resolve(hashed);
+                });
             });
+        };
+        const hashed = await hashPassword(req.body.password);
+        const user = new User({
+            username: req.body.username,
+            password: hashed,
         });
-    };
-    const hashed = await hashPassword(req.body.password);
-    const user = new User({
-        username: req.body.username,
-        password: hashed,
-    });
-    user.save()
-        .then(() => {
-            res.redirect("/sign-in");
-        })
-        .catch((err) => {
-            return next(err);
-        });
-};
+        user.save()
+            .then(() => {
+                res.redirect("/sign-in");
+            })
+            .catch((err) => {
+                return next(err);
+            });
+    },
+];
 passport.use(
     new LocalStrategy((username, password, done) => {
         User.findOne({ username: username })
